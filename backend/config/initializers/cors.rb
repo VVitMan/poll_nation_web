cbe14cc6e36b0104e49f5847a46601255f367b3e
VitItthikon <ithikorn.amornkhana@gmail.com>
2024-10-26 11:34:00 +0700

    edit port point to frontend 3001

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -18,7 +18,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:3000'  # Frontend URL
+      origins 'http://localhost:3001'  # Frontend URL port 3001 for development
       resource '*', headers: :any, methods: [:get, :post, :put, :delete]
     end
   end
